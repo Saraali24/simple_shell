d162a49c609e1dc167d2b3e84b121a1293cf905b
Saraali24 <saramabrouk.ali@gmail.com>
2023-08-19 13:23:59 -0700

    Simple Shell Task

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
 /**
  * main - Function that run my shell command-line interpreter .
- *
  * @argc: The number of command-line arguments.
- *
  * @argv: Array of strings containing the command-line arguments.
- *
  * Return: function returns (0).
-*/
-
+ */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 
 if (isatty(STDIN_FILENO) == 0)
 
-non_interactive(argv);
+my_non_interactive_mode(argv);
 
 else
 
-interactive(argv);
+my_interactive_mode(argv);
 
 return (0);
 }
